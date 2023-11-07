1362296889cb3e7ecbf6934981ed2f765901e99d
Daniel Tinivella <dtinivella@gmail.com>
2023-11-07 10:40:59 -0300

    add listener and manage connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let address = "127.0.0.1:8000";
+    let listener = TcpListener::bind(&address).unwrap();
+    println!("Server started {}", &address);
+   
+
+    // listening connections
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream); 
+    }
+}
+
+// manage connections
+fn handle_connection(mut stream: TcpStream){
+  let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Stream received!");
+    println!("{}", String::from_utf8_lossy(&buffer[..]));
 }
